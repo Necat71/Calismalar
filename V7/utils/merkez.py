0523f8c88500875c5a9c830bb917fee3020083e8
Necat71 <necatiakbulut67@gmail.com>
2026-01-15 07:16:55 +0300

    Delete V7 directory

--- V7/utils/merkez.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-
-def merkezle(pencere):
-    ekran = QApplication.primaryScreen().availableGeometry()
-    geo = pencere.frameGeometry()
-    x = ekran.center().x() - geo.width() // 2
-    y = ekran.center().y() - geo.height() // 2
-    pencere.move(x, y)
